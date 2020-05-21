02a66527ba2d2608453d18b87e6c1d29c250e618
FerielBelHadj <feriel.belhadjrhouma@gmail.com>
2020-05-21 10:48:38 +0200

    AlarmManager and Notifications

--- app/src/main/java/com/example/paranoidandroid/AlertReceiver.java
+++ app/src/main/java/com/example/paranoidandroid/AlertReceiver.java
@@ -12,7 +12,7 @@ public class AlertReceiver extends BroadcastReceiver {
             String medicineName = intent.getStringExtra("medicine");
 
             NotificationHelper notificationHelper = new NotificationHelper(context);
-            NotificationCompat.Builder nb = notificationHelper.getChannelNotification("Medicine Reminder", String.format(" It's time to take: $s. \n Please take care of yourself.  ", medicineName));
+            NotificationCompat.Builder nb = notificationHelper.getChannelNotification("Medicine Reminder", String.format(" It's time to take: %s. \n Please take care of yourself.  ", medicineName));
             notificationHelper.getManager().notify(1, nb.build());
         }
     }
